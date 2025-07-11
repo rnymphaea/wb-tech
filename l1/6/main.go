e12e69244a83e92eba5240f2cd86b2c7428e1bfb
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-07-11 16:42:59 +0300

    task l1.6: added new ways

--- l1/6/main.go
+++ l1/6/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "context"
+	"context"
 	"flag"
 	"fmt"
 	"log"
-	_ "runtime"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -23,6 +23,9 @@ func main() {
 
 	ConditionalExit()
 	ChannelExit()
+	ContextWithTimeoutExit()
+	ContextWithCancelExit()
+	RuntimeGoExit()
 }
 
 func ConditionalExit() {
@@ -50,7 +53,7 @@ func ConditionalExit() {
 			if debug {
 				log.Printf("[DEBUG] %s: [%vms] goroutine is working\n", funcName, time.Since(start).Milliseconds())
 			} else {
-				fmt.Printf("%s: goroutine is working...\n", funcName)
+				fmt.Printf("%s: goroutine is working\n", funcName)
 			}
 			time.Sleep(interval)
 		}
@@ -58,7 +61,7 @@ func ConditionalExit() {
 		if debug {
 			log.Printf("[DEBUG] %s: [%vms] goroutine: condition is no longer fulfilled\n", funcName, time.Since(start).Milliseconds())
 		} else {
-			fmt.Printf("%s: goroutine stops working...\n", funcName)
+			fmt.Printf("%s: goroutine stops working\n", funcName)
 		}
 	}()
 
@@ -100,16 +103,16 @@ func ChannelExit() {
 			select {
 			case <-stop:
 				if debug {
-					log.Printf("[DEBUG] %s: [%vms] goroutine received stop signal from channel\n", funcName, time.Since(start).Milliseconds())
+					log.Printf("[DEBUG] %s: [%vms] goroutine: received stop signal from channel\n", funcName, time.Since(start).Milliseconds())
 				} else {
-					fmt.Printf("%s: goroutine stops working...\n", funcName)
+					fmt.Printf("%s: goroutine stops working\n", funcName)
 				}
 				return
 			default:
 				if debug {
 					log.Printf("[DEBUG] %s: [%vms] goroutine is working\n", funcName, time.Since(start).Milliseconds())
 				} else {
-					fmt.Printf("%s: goroutine is working...\n", funcName)
+					fmt.Printf("%s: goroutine is working\n", funcName)
 				}
 				time.Sleep(interval)
 			}
@@ -125,5 +128,153 @@ func ChannelExit() {
 	}
 
 	stop <- struct{}{}
+	wg.Wait()
+}
+
+func ContextWithTimeoutExit() {
+	var (
+		wg       sync.WaitGroup
+		start    time.Time
+		interval time.Duration = 200 * time.Millisecond
+		timeout  time.Duration = 1 * time.Second
+		funcName string        = "func ContextWithTimeoutExit"
+	)
+
+	if debug {
+		log.Println("\nDemonstrating of exiting a goroutine with context.WithTimeout")
+		log.Printf("Interval between messages of goroutine: %v\n", interval)
+		log.Printf("Timeout: %v\n", timeout)
+		start = time.Now()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+
+		for {
+			select {
+			case <-ctx.Done():
+				if debug {
+					log.Printf("[DEBUG] %s: [%dms] goroutine: time exceeded\n", funcName, time.Since(start).Milliseconds())
+				} else {
+					fmt.Printf("%s: goroutine stops working\n", funcName)
+				}
+				return
+			default:
+				if debug {
+					log.Printf("[DEBUG] %s: [%dms] goroutine is working\n", funcName, time.Since(start).Milliseconds())
+				} else {
+					fmt.Printf("%s: goroutine is working\n", funcName)
+
+				}
+				time.Sleep(interval)
+			}
+		}
+	}(ctx)
+
+	if debug {
+		log.Printf("[DEBUG] %s: [%dms] sleeping for %v...\n", funcName, time.Since(start).Milliseconds(), timeout.Milliseconds())
+	}
+
+	time.Sleep(timeout)
+
+	wg.Wait()
+}
+
+func ContextWithCancelExit() {
+	var (
+		wg       sync.WaitGroup
+		start    time.Time
+		interval time.Duration = 200 * time.Millisecond
+		timeout  time.Duration = 1 * time.Second
+		funcName string        = "func ContextWithCancelExit"
+	)
+
+	if debug {
+		log.Println("\nDemonstrating of exiting a goroutine with context.WithCancel")
+		log.Printf("Interval between messages of goroutine: %v\n", interval)
+		log.Printf("Timeout: %v\n", timeout)
+		start = time.Now()
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+
+		for {
+			select {
+			case <-ctx.Done():
+				if debug {
+					log.Printf("[DEBUG] %s: [%dms] goroutine: received cancel signal\n", funcName, time.Since(start).Milliseconds())
+				} else {
+					fmt.Printf("%s: goroutine stops working...\n", funcName)
+				}
+				return
+			default:
+				if debug {
+					log.Printf("[DEBUG] %s: [%dms] goroutine is working\n", funcName, time.Since(start).Milliseconds())
+				} else {
+					fmt.Printf("%s: goroutine is working\n", funcName)
+
+				}
+				time.Sleep(interval)
+			}
+		}
+	}(ctx)
+
+	time.Sleep(timeout)
+	if debug {
+		log.Printf("[DEBUG] %s: [%dms] calling cancel()\n", funcName, time.Since(start).Milliseconds())
+	} else {
+		fmt.Printf("%s: calling cancel(). Quitting...\n", funcName)
+	}
+
+	cancel()
+	wg.Wait()
+}
+
+func RuntimeGoExit() {
+	var (
+		wg       sync.WaitGroup
+		start    time.Time
+		timeout  time.Duration = 1 * time.Second
+		funcName string        = "func RuntimeGoExit"
+	)
+
+	if debug {
+		log.Println("\nDemonstrating of exiting a goroutine with runtime.Goexit")
+		log.Printf("Timeout: %v\n", timeout)
+		start = time.Now()
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		if debug {
+			log.Printf("[DEBUG] %s: [%dms] entering to goroutine and sleeping for %v...\n", funcName, time.Since(start).Milliseconds(), timeout)
+		} else {
+			fmt.Printf("%s: entering to goroutine\n", funcName)
+		}
+
+		time.Sleep(timeout)
+
+		if debug {
+			log.Printf("[DEBUG] %s: [%dms] goroutine: quitting after sleep...\n", funcName, time.Since(start).Milliseconds())
+		} else {
+			fmt.Printf("%s: quitting goroutine after sleep...\n", funcName)
+		}
+
+		runtime.Goexit()
+	}()
+
+	if debug {
+		log.Printf("%s: [%dms] waiting for goroutine", funcName, time.Since(start).Milliseconds())
+	}
 	wg.Wait()
 }
