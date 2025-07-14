ad66d5bce7a89b90156ff0cf8e94040e13c097f6
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-07-14 15:51:56 +0300

    task l1.20: done

---
+++ l1/20/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var (
+		size int
+	)
+
+	fmt.Printf("Enter number of words: ")
+	fmt.Scan(&size)
+
+	s := make([]string, size)
+	fmt.Printf("Enter 3 words: ")
+
+	for i := 0; i < size; i++ {
+		fmt.Scan(&s[i])
+	}
+
+	result := ReverseWordsOrder(s)
+	fmt.Println("Result: ", result)
+}
+
+func ReverseWordsOrder(s []string) string {
+	l := len(s)
+
+	for i := 0; i < l/2; i++ {
+		s[i], s[l-i-1] = s[l-i-1], s[i]
+	}
+
+	return strings.Join(s, " ")
+}
