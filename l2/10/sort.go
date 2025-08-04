e4a04b4360e02000a701c242ee6d97f56ff88377
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-08-04 19:40:07 +0300

    task l2.10: added string scanning

--- l2/10/sort.go
+++ l2/10/sort.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	_ "strings"
 )
 
+const debugPrefix = "[DEBUG]"
+
 var (
-	debug  bool
-	check  bool
-	unique bool
+	debug    bool
+	check    bool
+	unique   bool
+	filepath string
 )
 
 type sortOptions struct {
@@ -26,6 +31,7 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "show logs to check the correctness of execution")
 	flag.BoolVar(&check, "c", false, "check whether the given text is already sorted")
 	flag.BoolVar(&unique, "u", false, "print unique strings only")
+	flag.StringVar(&filepath, "file", "", "sort lines from specified file")
 
 	var opts sortOptions
 	flag.IntVar(&opts.key, "k", 1, "specify a sort field")
@@ -43,10 +49,78 @@ func main() {
 		log.Println("Debug mode started")
 	}
 
+	var (
+		input    []string
+		inputErr error
+	)
+
+	if len(filepath) > 0 {
+		input, inputErr = readLinesFromFile(filepath)
+		if inputErr != nil {
+			fmt.Println("error:", inputErr)
+			return
+		}
+	} else {
+		var size int
+		fmt.Printf("Enter the number of strings: ")
+		fmt.Scan(&size)
+
+		input, inputErr = readLinesFromStdin(size)
+		if inputErr != nil {
+			fmt.Println("error:", inputErr)
+			return
+		}
+	}
+
+	if debug {
+		log.Printf("%s lines:\n", debugPrefix)
+		for i := 0; i < len(input); i++ {
+			log.Println(input[i])
+		}
+	}
+
 	if err := validateOpts(&opts); err != nil {
 		fmt.Println("error:", err)
 		return
 	}
+
+	sort(input, &opts)
+}
+
+func readLinesFromFile(filepath string) ([]string, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+func readLinesFromStdin(size int) ([]string, error) {
+	lines := make([]string, size)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("Enter %d lines that you want to sort: ", size)
+
+	for i := 0; i < size; i++ {
+		scanner.Scan()
+		line := scanner.Text()
+
+		if len(line) == 0 {
+			break
+		} else {
+			lines[i] = line
+		}
+	}
+
+	return lines, scanner.Err()
 }
 
 func validateOpts(opts *sortOptions) error {
@@ -60,4 +134,8 @@ func validateOpts(opts *sortOptions) error {
 	} else {
 		return fmt.Errorf("mutually exclusive flags")
 	}
+}
+
+func sort(arr []string, opts *sortOptions) []string {
+	return arr
 }
